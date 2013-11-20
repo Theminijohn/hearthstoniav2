4534f36d71a11ca21e4aeb1cd05a1b5e63a36214
The Mini John <admin@phoenix-prime.gr>
2013-11-20 04:22:52 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+== Hearthstonia v2
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hearthstoniav2::Application.config.secret_key_base = '33e391d170d861843bbb4b99c3e9f09b5910aa4fe604befce7c3814167143b65ca22ebca409aab089d8ccc5c7d18d51ef27997b208b56b5d6736d5264d99e720'
