4655f37b903421b00297c4710eed38005bf4e7b9
The Mini John <admin@phoenix-prime.gr>
2013-11-23 07:16:33 +0100

    Redactor Configs

--- app/assets/javascripts/redactor-rails/config.js
+++ app/assets/javascripts/redactor-rails/config.js
@@ -7,11 +7,9 @@ $(document).ready(
     params = csrf_param + "=" + encodeURIComponent(csrf_token);
   }
   $('.redactor').redactor(
-    { "imageUpload":"/redactor_rails/pictures?" + params,
-      "imageGetJson":"/redactor_rails/pictures",
-      "fileUpload":"/redactor_rails/documents?" + params,
-      "fileGetJson":"/redactor_rails/documents",
-      "path":"/assets/redactor-rails",
-      "css":"style.css"}
+    { buttons: [ 'bold', 'italic', 'deleted', '|', 'table', 'link', '|',
+                 'alignment', '|', 'unorderedlist', 'orderedlist', '|',
+                 'horizontalrule'], "css":"redactor-style.css.scss",
+                 toolbarFixed: true }
   );
 });
