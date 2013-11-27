47411192276ed81a508d7df592ae4611d6306dae
The Mini John <admin@phoenix-prime.gr>
2013-11-27 16:11:58 +0100

    ss

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
 
-  validates :body, :presence => true
+  validates :body, :presence => true, length: { minimum: 15 }
   validates :question_id, :presence => true
   validates :user_id, :presence => true
 end
